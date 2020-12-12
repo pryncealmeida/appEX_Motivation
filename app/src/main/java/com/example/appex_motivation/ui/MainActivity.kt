54d2e55d4f0e201d628ea8ae90c7b7d2861fdb77
Prynce <pryncealmeida@gmail.com>
2020-12-12 12:21:15 -0300

    Commit 1054 12122020

--- app/src/main/java/com/example/appex_motivation/ui/MainActivity.kt
+++ app/src/main/java/com/example/appex_motivation/ui/MainActivity.kt
@@ -1,11 +1,15 @@
-package com.example.appex_motivation
+package com.example.appex_motivation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.appex_motivation.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (supportActionBar != null){
+            supportActionBar!!.hide()
+        }
     }
 }
---
+++ app/src/main/java/com/example/appex_motivation/ui/SplashActivity.kt
@@ -0,0 +1,46 @@
+package com.example.appex_motivation.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.appex_motivation.R
+import com.example.appex_motivation.infra.SecurityPreferences
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var mSecurityPreferences: SecurityPreferences
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        mSecurityPreferences = SecurityPreferences(this)
+
+        if (supportActionBar != null) {
+            supportActionBar!!.hide()
+        }
+
+        btnSave.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        val id = v.id
+        if (id == R.id.btnSave) {
+            handleSave()
+        }
+    }
+
+    private fun handleSave() {
+      val name = etext1.text.toString()
+        if (name != "") {
+            mSecurityPreferences.storeString("name", name)
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, "Preencha com seu nome!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}

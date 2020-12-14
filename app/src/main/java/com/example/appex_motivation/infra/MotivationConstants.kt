86c8ded5eac06177501e6b8ce66f715048eb10a7
Prynce <pryncealmeida@gmail.com>
2020-12-14 15:17:53 -0300

    Commit 1517 14122020

--- app/src/main/java/com/example/appex_motivation/infra/MotivationConstants.kt
+++ app/src/main/java/com/example/appex_motivation/infra/MotivationConstants.kt
@@ -5,4 +5,13 @@ class MotivationConstants private constructor() {
     object KEY {
         val PERSON_NAME = "name"
     }
+
+    object PHRASEFILTER {
+
+        val ALL = 1
+        val SMILE = 2
+        val SUNNY = 3
+
+
+    }
 }
--- app/src/main/java/com/example/appex_motivation/ui/MainActivity.kt
+++ app/src/main/java/com/example/appex_motivation/ui/MainActivity.kt
@@ -2,12 +2,13 @@ package com.example.appex_motivation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.appex_motivation.R
 import com.example.appex_motivation.infra.MotivationConstants
 import com.example.appex_motivation.infra.SecurityPreferences
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var mSecurityPreferences: SecurityPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,11 +16,58 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         mSecurityPreferences = SecurityPreferences(this)
-        t1_main.text = "Olá, " + mSecurityPreferences.getString(MotivationConstants.KEY.PERSON_NAME).toString()
+        t1_main.text =
+            "Olá, " + mSecurityPreferences.getString(MotivationConstants.KEY.PERSON_NAME).toString()
 
 
         if (supportActionBar != null) {
             supportActionBar!!.hide()
         }
+
+        btn_newPhrase.setOnClickListener(this)
+        img_infinit.setOnClickListener(this)
+        img_smile.setOnClickListener(this)
+        img_sunny.setOnClickListener(this)
+
+
+    }
+
+    override fun onClick(view: View) {
+
+        val idv = view.id
+        val listFilter = listOf(R.id.img_infinit, R.id.img_smile, R.id.img_sunny)
+
+        if (idv == R.id.btn_newPhrase) {
+            handleNewPhrase()
+        } else if (idv in listFilter) {
+            handleFilter(idv)
+        }
+
+
+    }
+
+    private fun handleFilter(idv: Int) {
+        img_infinit.setColorFilter(resources.getColor(R.color.white))
+        img_smile.setColorFilter(resources.getColor(R.color.white))
+        img_sunny.setColorFilter(resources.getColor(R.color.white))
+
+
+        when (idv) {
+            R.id.img_infinit -> {
+                img_infinit.setColorFilter(resources.getColor(R.color.colorAccent))
+            }
+            R.id.img_smile -> {
+                img_smile.setColorFilter(resources.getColor(R.color.colorAccent))
+            }
+            R.id.img_sunny -> {
+                img_sunny.setColorFilter(resources.getColor(R.color.colorAccent))
+            }
+        }
+
+
+    }
+
+    private fun handleNewPhrase() {
+
     }
 }
